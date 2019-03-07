892f00d503e155872e0a8d34113d2f74f9c306fe
jingtian_zhang <jtz9317@gmail.com>
2019-03-06 18:14:36 -0800

    changes

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Jumbotron from "./components/Jumbotron";
+import React, { Component } from "react";
 import Card from "./components/Card";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Title from "./components/Title";
+import cards from "./cards.json";
 
-function App() {
-  return (
-    <div className="container">
-      <Navbar />
-      <Jumbotron />
-      <Card />
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.cards to the cards.json array
+  state = {
+    cards
+  };
+
+  render() {
+    return (
+      <Header />
+      <Title />
+      {this.state.cards.map(card => (
+        <Cards
+          image={card.image}
+        />
+      ))
+    }
+    <Footer />
+    )
+  }
 }
 
 export default App;
---
+++ clickygame/src/components/Navbar/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import "../Navbar/style.css";
+
+// By importing the Navbar.css file, it is added to the DOM whenever this component loads
+
+// We can also style a component inside of its JavaScript file by adding style properties to its rendered elements
+// Unlike regular HTML, a JSX style property must be an object instead of a string
+// On a style object, we camelCase all property names, and put all of the values in quotes
+// Non quoted values default to "pixels", e.g. height, margin, padding
+
+const styles = {
+  navbarStyle: {
+    background: "green",
+    justifyContent: "flex-end"
+  }
+};
+
+// We use JSX curly braces to evaluate the style object on the JSX tag
+
+function Navbar() {
+  return (
+    <nav style={styles.navbarStyle} className="navbar">
+      <ul>
+        <li className="brand">
+          <a href="/">Clicky Game</a>
+        </li>
+        <li>You Guessed Incorrectly!</li>
+        <li>
+          Score:{this.state.score}
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
